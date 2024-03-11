f0ca8aaaeff803eb5934a9d6766e95824d9fe8c7
Marcel Ullrich <ullrich@cs.uni-saarland.de>
2024-03-11 09:24:09 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/index.tsx</code> and save to reload.
+          Check the console for the output of the Z3 solver.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/z3-test.ts
@@ -0,0 +1,41 @@
+
+import * as z3 from 'z3-solver';
+
+console.log("Starting Z3");
+declare global {
+  interface Window { z3Promise: ReturnType<typeof z3.init>; }
+}
+window.z3Promise = z3.init();
+(async () => {
+  console.log('### running the low-level API')
+  let { Z3 } = await window.z3Promise;
+  let config = Z3.mk_config();
+  let ctx = Z3.mk_context_rc(config);
+  Z3.del_config(config);
+  let command = `
+    (declare-const a Int)
+    (declare-fun f (Int Bool) Int)
+    (assert (< a 10))
+    (assert (< (f a true) 100))
+    (check-sat)
+    (get-model)
+  `;
+  console.log(await Z3.eval_smtlib2_string(ctx, command));
+  Z3.del_context(ctx);
+
+
+  
+  console.log('')
+  console.log('### running the high-level API')
+  let { Context } = await window.z3Promise;
+  let { Solver, Int } = Context('main');
+  let solver = new Solver();
+  let x = Int.const('x');
+  solver.add(x.add(5).eq(9));
+  console.log(await solver.check());
+  console.log('x is', solver.model().get(x).toString());
+
+
+})().catch(e => {
+  console.error(e);
+});
